549e76ab873ebe83f469de1c07b43bf245f442ff
Ostap Hapyak <ostaphapyak@gmail.com>
2025-07-26 21:57:54 +0300

    Lesson_6_GIT file updated with new commands

--- Ostap_Hapyak/Lesson_6_GIT.py
+++ Ostap_Hapyak/Lesson_6_GIT.py
@@ -1,10 +1,23 @@
 #Команди для роботи з GIT
 
 # git init
-# git config --global user.name "Твоє Ім'я"
-# git config --global user.email "твій@email.com"
+# git config --global user.name "Your name"
+# git config --global user.email "Your@email.com"
 # git config --global core.editor notepad.exe
 # git config --list
 # git status
 # git add file_name
-
+# git add . or git add * (the same)
+# git commit -m "Message about commit"
+# git commit -a -m "Message about commit"  (to do commit with add)
+# git log  (to see list of commits)
+# git checkout commit_ID
+# git checkout main  (return to last commit)
+# git branch
+# git branch branch_name
+# git checkout branch_name
+# git rm file_name  (remove files from the working tree and from the index)
+# git diff  (to see what is modified but unstaged in repo)
+# git diff HEAD  (to see difference between committed working repo and current stage of the repo - workspace)
+# git diff --staged / cached  (to see a list of staged changes)
+# git diff file.py  (to see changes in the file)
--- main.py
+++
@@ -1,9 +0,0 @@
-import random
-
-user_input = int(input("Введіть число від 0 до 100: "))
-random_number = random.randint(0, 100)
-
-print("Виграшне число:", random_number)
-
-if user_input == random_number:
-    print("Вітаю! Ви виграли!")
