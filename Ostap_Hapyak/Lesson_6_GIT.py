775bc2c7d7b36e4e10f3979bc10bab046742a3fe
Ostap Hapyak <ostaphapyak@gmail.com>
2025-07-27 21:38:00 +0300

    added Zen of Python.txt; Lesson_6_GIT updated with new commands

--- Ostap_Hapyak/Lesson_6_GIT.py
+++ Ostap_Hapyak/Lesson_6_GIT.py
@@ -1,23 +1,34 @@
-#Команди для роботи з GIT
+"""
+Команди для роботи з GIT
 
-# git init
-# git config --global user.name "Your name"
-# git config --global user.email "Your@email.com"
-# git config --global core.editor notepad.exe
-# git config --list
-# git status
-# git add file_name
-# git add . or git add * (the same)
-# git commit -m "Message about commit"
-# git commit -a -m "Message about commit"  (to do commit with add)
-# git log  (to see list of commits)
-# git checkout commit_ID
-# git checkout main  (return to last commit)
-# git branch
-# git branch branch_name
-# git checkout branch_name
-# git rm file_name  (remove files from the working tree and from the index)
-# git diff  (to see what is modified but unstaged in repo)
-# git diff HEAD  (to see difference between committed working repo and current stage of the repo - workspace)
-# git diff --staged / cached  (to see a list of staged changes)
-# git diff file.py  (to see changes in the file)
+git init
+git config --global user.name "Your name"
+git config --global user.email "Your@email.com"
+git config --global core.editor notepad.exe
+git config --list
+git remote -v  (check remote repo)
+git status
+git add file_name
+git add . or git add * (the same)
+git commit
+git commit -m "Message about commit"
+git commit -a -m "Message about commit"  (to do commit with add)
+git log  (to see list of commits)
+git checkout commit_ID
+git checkout main  (return to last commit)
+git branch
+git branch branch_name
+git branch -d branch_name  (delete the branch)
+git branch -m branch_name  (rename the branch)
+git checkout branch_name
+git rm file_name  (remove files from the working tree and from the index)
+git diff  (to see what is modified but unstaged in repo)
+git diff HEAD  (to see difference between committed working repo and current stage of the repo - workspace)
+git diff --staged / cached  (to see a list of staged changes)
+git diff file.py  (to see changes in the file)
+git merge branch name (to do merge to the current branch from another one)
+git pull origin main
+git push origin main
+
+"""
+
